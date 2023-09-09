50cfbb0c785ebab77a666493e08285e852eca2aa
Hayunho <yunho081544@gmail.com>
2023-09-10 01:30:17 +0900

    플레이어 중력 추가
    - Layer 10번 Ground로 설정
    - 중력 적용

--- CapstoneProject/Assets/Test/Ha/Scripts/PlayerMovement.cs
+++ CapstoneProject/Assets/Test/Ha/Scripts/PlayerMovement.cs
@@ -6,7 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    // 속도
     public float speed = 12f;
+    public float gravity = -9.8f;
+
+    // GroundCheck 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +27,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        // 중력 y값 변화량
+        velocity.y += gravity * Time.deltaTime;
+
+        // △y = 1/2 * g * time**2
+        controller.Move(velocity * Time.deltaTime);
     }
 }
