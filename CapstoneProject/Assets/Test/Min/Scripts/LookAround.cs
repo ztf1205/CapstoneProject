4b8d357215b40b6fd821acdbaf894e0bfbef7129
Minchae Kim <yjk199900@gmail.com>
2023-09-12 20:44:57 +0900

    fluid 테스트

---
+++ CapstoneProject/Assets/Test/Min/Scripts/LookAround.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(Camera))]
+public class LookAround : MonoBehaviour
+{
+    public struct CameraShot
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 up;
+        public float filedOfView;
+
+        public CameraShot(Vector3 position, Quaternion rotation, Vector3 up, float filedOfView)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.up = up;
+            this.filedOfView = filedOfView;
+        }
+    }
+
+    private Camera cam;
+    private CameraShot currentShot;
+
+    public float movementSpeed = 5;
+    public float rotationSpeed = 8;
+    public float translationResponse = 10;
+    public float rotationResponse = 10;
+    public float fovResponse;
+
+    private void Awake() 
+    {
+        cam = GetComponent<Camera>();
+        currentShot = new CameraShot(transform.position, transform.rotation, transform.up, cam.fieldOfView);
+    }
+
+    private void LookAt(Vector3 position, Vector3 up)
+    {
+        currentShot.up = up;
+        currentShot.rotation = Quaternion.LookRotation(position - currentShot.position, currentShot.up);
+    }
+
+    private void UpdateShot()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentShot.position, translationResponse*Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentShot.rotation, rotationResponse*Time.deltaTime);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, currentShot.filedOfView, fovResponse*Time.deltaTime);
+    }
+    
+    private void LateUpdate() 
+    {
+        Vector3 delta = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            delta += cam.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            delta -= cam.transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            delta -= cam.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            delta += cam.transform.right;
+        }
+
+        currentShot.position += delta * Time.deltaTime * movementSpeed;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            float deltaX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float deltaY = Input.GetAxis("Mouse Y") * rotationSpeed;
+            Quaternion fwd = currentShot.rotation * Quaternion.AngleAxis(deltaX, Vector3.up) * Quaternion.AngleAxis(deltaY, - Vector3.right);
+            LookAt(currentShot.position + fwd * Vector3.forward, Vector3.up);
+        }
+
+        UpdateShot();
+    }
+}
