428422c7bf3ad7235a500da5ed46397fd02f2e06
Minchae Kim <yjk199900@gmail.com>
2023-11-12 01:02:55 +0900

    Resizable Object 수정
    - BoxCollider, CapsuleCollider 두 가지 경우 분리
    - 2D일 때 콜라이더 비활성화 기능 추가

--- CapstoneProject/Assets/Scripts/Objects/ResizableObject.cs
+++ CapstoneProject/Assets/Scripts/Objects/ResizableObject.cs
@@ -1,16 +1,42 @@
+using Obi;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResizableObject : MonoBehaviour
 {
-    private BoxCollider boxCollider;
+    private Collider collider;
+
+    private BoxCollider boxCollider = null;
+    private CapsuleCollider capsuleCollider = null;
+
     private Vector3 originalSize;
+    private float originalHeight;
+
+    [SerializeField] private bool isCollision2DEnabled = true;
+
+    private DimensionManager dimManager;
 
     private void Awake()
     {
-        boxCollider = GetComponent<BoxCollider>();
-        originalSize = boxCollider.size;
+        Init();
+    }
+
+    private void Init()
+    {
+        collider = GetComponent<Collider>();
+        dimManager = GameObject.Find("DimensionManager").GetComponent<DimensionManager>();
+
+        if (collider is BoxCollider)
+        {
+            boxCollider = (BoxCollider)collider;
+            originalSize = boxCollider.size;
+        }
+        else if (collider is CapsuleCollider)
+        {
+            capsuleCollider = (CapsuleCollider)collider;
+            originalHeight = capsuleCollider.height;
+        }
     }
 
     private void Start()
@@ -19,14 +45,30 @@ public class ResizableObject : MonoBehaviour
         EventManager.Subscribe("ResetCollider", ResetCollider);
     }
 
+    private void Update()
+    {
+        if (dimManager.Is2D)
+            collider.enabled = isCollision2DEnabled;
+        else
+            collider.enabled = true;
+    }
+
     private void ResizeCollider()
     {
-        boxCollider.size = new Vector3(originalSize.x, originalSize.y, 999999f);
+        if (boxCollider != null)
+            boxCollider.size = new Vector3(originalSize.x, originalSize.y, 999999f);
+
+        if (capsuleCollider != null)
+            capsuleCollider.height = 999999f;
     }
 
     private void ResetCollider()
     {
-        boxCollider.size = originalSize;
+        if (boxCollider != null)
+            boxCollider.size = originalSize;
+
+        if (capsuleCollider != null)
+            capsuleCollider.height = originalHeight;
     }
 
     private void OnDestroy()
