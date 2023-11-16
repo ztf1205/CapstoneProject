c718627324669c9bd5036ccffed5ae50c787b2cd
Minchae Kim <yjk199900@gmail.com>
2023-11-16 17:14:23 +0900

    Cube, CubeHolder 프리팹 작업 완료
    - Crystal은 획득 시 CrystalHolder 위치로 옮기는 방식
    - Cube는 획득 시 CubeHolder 위에 있는 큐브가 활성화되는 방식

---
+++ CapstoneProject/Assets/Scripts/Objects/Cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private static int count = 0;
+
+    public static int Count() { return count; }
+    public static void IncreaseCount() { count++; }
+    public static void DecreaseCount() { count--; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IncreaseCount();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ CapstoneProject/Assets/Scripts/Objects/CubeHolder.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHolder : SignalGenerator
+{
+    [SerializeField] private GameObject cube;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Cube.Count() > 0)
+        {
+            GeneratorActivate();
+        }
+    }
+
+    protected override void GeneratorActivate()
+    {
+        base.GeneratorActivate();
+        Cube.DecreaseCount();
+        cube.SetActive(true);
+    }
+
+    protected override void GeneratorDeactivate()
+    {
+        base.GeneratorDeactivate();
+    }
+}
