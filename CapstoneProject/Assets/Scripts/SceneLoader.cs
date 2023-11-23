318cd5c9ee63c2bc0c5a5bffbfac4e9372f4f271
Yohan <ztf1205@naver.com>
2023-11-23 20:18:35 +0900

    로고 추가 및 UI 수정
    -메인 화면에 로고 추가
    -메인 화면과 게임 클리어 화면 UI 수정

--- CapstoneProject/Assets/Scripts/SceneLoader.cs
+++ CapstoneProject/Assets/Scripts/SceneLoader.cs
@@ -9,4 +9,15 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(sceneNameToLoad);
     }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        // 에디터 모드에서는 플레이 모드를 중지합니다.
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 빌드된 런타임에서는 어플리케이션을 종료합니다.
+        Application.Quit();
+#endif
+    }
 }
