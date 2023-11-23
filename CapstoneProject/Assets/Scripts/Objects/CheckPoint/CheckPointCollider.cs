ea813dc617e32b52543f4f6dd75fc7ae353464b9
Minchae Kim <yjk199900@gmail.com>
2023-11-24 00:49:52 +0900

    CheckPoint Collider, CheckPoint Flag 프리팹 추가

--- CapstoneProject/Assets/Scripts/Objects/CheckPoint/CheckPointCollider.cs
+++ CapstoneProject/Assets/Scripts/Objects/CheckPoint/CheckPointCollider.cs
@@ -6,6 +6,8 @@ public class CheckPointCollider : MonoBehaviour
 {
     private GameObject checkPoint;
 
+    [SerializeField] private GameObject checkPointFlag;
+
     private void Awake()
     {
         checkPoint = GameObject.FindGameObjectWithTag("CheckPoint");
@@ -16,6 +18,9 @@ public class CheckPointCollider : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             checkPoint.transform.position = transform.position;
+
+            gameObject.SetActive(false);
+            checkPointFlag.SetActive(true);
         }
     }
 }
