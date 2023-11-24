b68890e3ba73f7847165d4b69e9bc212a68be14a
Minchae Kim <yjk199900@gmail.com>
2023-11-24 16:29:52 +0900

    Main 씬 수정, 스킬 획득 UI 이벤트 추가

--- CapstoneProject/Assets/Scripts/TextManager.cs
+++ CapstoneProject/Assets/Scripts/TextManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class TextManager : MonoBehaviour
 {
+    [SerializeField] private Canvas canvas;
     private TextMeshProUGUI text;
 
     private void Awake()
@@ -16,30 +17,39 @@ public class TextManager : MonoBehaviour
     {
         EventManager.Subscribe("ItemTextOn", ItemTextOn);
         EventManager.Subscribe("SwitchTextOn", SwitchTextOn);
+        EventManager.Subscribe("SkillTextOn", SkillTextOn);
         EventManager.Subscribe("TextOff", TextOff);
     }
 
     private void ItemTextOn()
     {
         text.SetText("아이템을 사용하려면[E] 키를 누르세요.");
-        text.enabled = true;
+        canvas.enabled = true;
     }
 
     private void SwitchTextOn()
     {
         text.SetText("[E] 키를 눌러 상호작용하세요.");
-        text.enabled = true;
+        canvas.enabled = true;
+    }
+
+    private void SkillTextOn()
+    {
+        text.SetText("차원 스위치 스킬을 획득하였습니다!" + "\n" + "마우스 왼쪽 버튼을 클릭해보세요.");
+        canvas.enabled = true;
+        Invoke("TextOff", 3f);
     }
 
     private void TextOff()
     {
-        text.enabled = false;
+        canvas.enabled = false;
     }
 
     private void OnDestroy()
     {
         EventManager.Unsubscribe("ItemTextOn", ItemTextOn);
         EventManager.Unsubscribe("SwitchTextOn", SwitchTextOn);
+        EventManager.Unsubscribe("SkillTextOn", SkillTextOn);
         EventManager.Unsubscribe("TextOff", TextOff);
     }
 }
